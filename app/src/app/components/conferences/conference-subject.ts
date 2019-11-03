17b9df0c64766ed869d1e40fd10b5ec3e78767cc
Willem Houm <willem.houm@gmail.com>
2019-11-03 15:22:58 +0000

    add id for object

--- app/src/app/components/conferences/conference-subject.ts
+++ app/src/app/components/conferences/conference-subject.ts
@@ -1,5 +1,7 @@
 export class ConferenceSubject {
+    idConferenceSubject: number;
     name: string;
     description: string;
     hour: string;
+    // markDown: string;
 }
--- app/src/app/components/conferences/conference.ts
+++ app/src/app/components/conferences/conference.ts
@@ -1,6 +1,7 @@
 import { ConferenceSubject } from "./conference-subject";
 
 export class Conference {
+    idConference: number;
     name: string;
     description: string;
     subjectList: ConferenceSubject[];
